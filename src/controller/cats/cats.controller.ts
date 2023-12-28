989948379b6b3d8ce446305c4902fdecf4197258
Samuil Berenzon <samus.dev.space@gmail.com>
2023-12-28 19:42:25 +0100

    cats bp test

--- src/controller/cats/cats.controller.ts
+++ src/controller/cats/cats.controller.ts
@@ -1,11 +1,16 @@
 //---------IMPORTS------------\
 
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 
+//---------MAIN------------\
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  findAllCats(): { message: string } {
+    return { message: 'This action returns all cats' };
+  }
+  @Post()
+  createCat(@Body() body: { name: string }): string {
+    return `This action adds a new cat ${body.name}`;
   }
 }
