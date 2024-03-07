178dce83fc4f2ecdb1b65833d3e9a67d4e7bf15d
Samuil Berenzon <samus.dev.space@gmail.com>
2024-03-08 00:21:14 +0100

    commented route tree print

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(4000);
-  const server = app.getHttpServer();
-  const router = server._events.request._router;
+  // const server = app.getHttpServer();
+  // const router = server._events.request._router;
 
-  const availableRoutes = router.stack
-    .filter((r) => r.route)
-    .map((r) => {
-      return {
-        method: Object.keys(r.route.methods)[0].toUpperCase(),
-        path: r.route.path,
-      };
-    });
-  console.log(availableRoutes);
+  // const availableRoutes = router.stack
+  //   .filter((r) => r.route)
+  //   .map((r) => {
+  //     return {
+  //       method: Object.keys(r.route.methods)[0].toUpperCase(),
+  //       path: r.route.path,
+  //     };
+  //   });
+  // console.log(availableRoutes);
 }
 bootstrap();
