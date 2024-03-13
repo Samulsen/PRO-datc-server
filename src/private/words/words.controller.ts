f9714a99c55cc952dde9e38d28e47c3d274b42a7
Samuil Berenzon <samus.dev.space@gmail.com>
2024-03-13 22:29:00 +0100

    added logic to controller with validation adn error handlign

--- src/private/words/words.controller.ts
+++ src/private/words/words.controller.ts
@@ -1,11 +1,47 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Patch,
+  Param,
+  Delete,
+  Body,
+  ValidationPipe,
+  UsePipes,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { CreateWordDto, UpdateWordDto } from 'src/private/words/words.dto';
 import { WordsService } from 'src/private/words/words.service';
 
 @Controller('words')
 export class WordsController {
   constructor(private readonly wordsService: WordsService) {}
+
   @Post()
-  getHello(): string {
-    return this.wordsService.getHello();
+  @UsePipes(ValidationPipe)
+  async create(@Body() newWord: CreateWordDto) {
+    try {
+      return await this.wordsService.createWord(newWord);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @Patch(':word')
+  async update(@Body() update: UpdateWordDto, @Param('word') word: string) {
+    try {
+      return await this.wordsService.updateWord(update, word);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @Delete(':word')
+  async delete(@Param('word') word: string) {
+    try {
+      return await this.wordsService.deleteWord(word);
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
   }
 }
