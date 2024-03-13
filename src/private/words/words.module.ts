b998aec9eeeb6073182f612b678358ef57bffe63
Samuil Berenzon <samus.dev.space@gmail.com>
2024-03-13 22:34:17 +0100

    fixed import

--- src/private/words/words.module.ts
+++ src/private/words/words.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { WordsController } from 'src/private/words/words.controller';
 import { WordsService } from 'src/private/words/words.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MWord, WordSchema } from 'src/private/words/words.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature(
+      [{ name: MWord.name, schema: WordSchema }],
+      'dictDB',
+    ),
+  ],
   controllers: [WordsController],
   providers: [WordsService],
 })
