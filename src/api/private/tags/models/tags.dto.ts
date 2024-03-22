1ad07dc7840fe48b1cf7ea16c64b65a6a485a09c
Samuil Berenzon <samus.dev.space@gmail.com>
2024-03-22 22:09:36 +0100

    checked message in, need changes for destructuring

--- src/api/private/tags/models/tags.dto.ts
+++ src/api/private/tags/models/tags.dto.ts
@@ -1,10 +1,11 @@
 import { IsEnum, IsString } from 'class-validator';
 import { ETagsGroup } from 'src/api/private/tags/models/tags.types';
+import { tagsStringUtilsInvalidGroupMessage as invalidGroupMessage } from 'src/api/private/tags/helpers/tags.string.utils';
 
 export class CreateTagDto {
   @IsString()
   name: string;
-
-  @IsEnum(ETagsGroup)
+  ////__NOTE: check here
+  @IsEnum(ETagsGroup, { message: invalidGroupMessage.bind(null) })
   group: ETagsGroup;
 }
