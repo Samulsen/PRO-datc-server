119f6a0f51dceee9b2f62b320c4abf6c37e5d3db
Samuil Berenzon <samus.dev.space@gmail.com>
2024-03-24 18:07:41 +0100

    added mongoose config

--- src/api/private/concepts/concepts.module.ts
+++ src/api/private/concepts/concepts.module.ts
@@ -2,8 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConceptsController } from 'src/api/private/concepts/concepts.controller';
 import { ConceptsService } from 'src/api/private/concepts/concepts.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Concept,
+  ConceptSchema,
+} from 'src/api/private/concepts/models/concepts.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature(
+      [{ name: Concept.name, schema: ConceptSchema }],
+      'dictDB',
+    ),
+  ],
   controllers: [ConceptsController],
   providers: [ConceptsService],
 })
