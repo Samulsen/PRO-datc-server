da0e0dd5f0ea45f4e4b633852deae03b1009ec51
Samuil Berenzon <samus.dev.space@gmail.com>
2024-03-24 19:07:33 +0100

    added flag

--- src/api/private/concepts/concepts.controller.ts
+++ src/api/private/concepts/concepts.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, Get } from '@nestjs/common';
 import { ConceptsService } from 'src/api/private/concepts/concepts.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('concepts')
 @Controller('concepts')
 export class ConceptsController {
   constructor(private readonly conceptsService: ConceptsService) {}
