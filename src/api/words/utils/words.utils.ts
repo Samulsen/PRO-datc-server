95565d4dfb6a0fcf5b0f169e305d9329c8b8c48a
Samuil Berenzon <samus.dev.space@gmail.com>
2024-04-01 22:31:38 +0200

    made validation, needs seperation for next step

---
+++ src/api/words/utils/words.utils.ts
@@ -0,0 +1,88 @@
+import { CreateWordDto } from "src/api/words/models/words.dto";
+import { Model } from "mongoose";
+import { WordDocument, WordSchema } from "src/api/words/models/words.schema";
+import { stringUtilsNotExistsMessage as notExistMessage } from "src/utils/strings.utils";
+import { MFailureResponse, MSuccessResponse } from "src/types/responses.types";
+
+type WordModel = Model<WordDocument>;
+type ErrorObject = { origin: string; message: string };
+enum WordProps {
+  WORD = "word",
+  CONCEPT = "concept",
+  COMBINATOR = "combinator",
+  VARIANT = "variant",
+  SYNONYM = "synonym",
+  ANTAGONIST = "antagonist",
+}
+
+type SuccessResponse = MSuccessResponse<CreateWordDto, typeof WordSchema>;
+type FailureResponse = MFailureResponse<CreateWordDto, ErrorObject>;
+
+const validateWordsExistenceFromProperty = async (
+  words: string[],
+  property: WordProps,
+  wordModel: WordModel,
+): Promise<{
+  property: WordProps;
+  hasErrors: boolean;
+  errors?: ErrorObject[];
+}> => {
+  const wordsExistence = await Promise.all(
+    words.map(async (word) => {
+      return {
+        wordDoc: wordModel.findOne({
+          value: word,
+        }),
+        target: word,
+      };
+    }),
+  );
+
+  const errors = wordsExistence
+    .filter((word) => !word.wordDoc)
+    .map(({ target }) => {
+      return {
+        origin: target,
+        message: notExistMessage(property, target),
+      };
+    });
+
+  return {
+    property: property,
+    hasErrors: errors.length > 0,
+    errors: errors.length > 0 ? errors : undefined,
+  };
+};
+
+export const wordsUtilValidatePayloadValues = async (
+  wordModel: WordModel,
+  newWord: CreateWordDto,
+): Promise<SuccessResponse | FailureResponse> => {
+  const { value, combinators, concepts, antagonists, variants, synonyms } =
+    newWord;
+
+  const validateAllProperties = await Promise.all([
+    validateWordsExistenceFromProperty([value], WordProps.WORD, wordModel),
+    validateWordsExistenceFromProperty(
+      combinators,
+      WordProps.COMBINATOR,
+      wordModel,
+    ),
+    validateWordsExistenceFromProperty(concepts, WordProps.CONCEPT, wordModel),
+    validateWordsExistenceFromProperty(
+      antagonists,
+      WordProps.ANTAGONIST,
+      wordModel,
+    ),
+    validateWordsExistenceFromProperty(variants, WordProps.VARIANT, wordModel),
+    validateWordsExistenceFromProperty(synonyms, WordProps.SYNONYM, wordModel),
+  ]);
+
+  const errors = validateAllProperties
+    .filter((validationObject) => validationObject.hasErrors)
+    .map((validationObject) => validationObject.errors);
+
+  if (errors.length > 0) {
+    // return {Input: newWord, Output:};
+  }
+};
--- src/api/words/words.service.ts
+++ src/api/words/words.service.ts
@@ -1,25 +1,25 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Word, WordDocument } from 'src/api/words/models/words.schema';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Word, WordDocument } from "src/api/words/models/words.schema";
 import {
   stringUtilExistsMessage as existMessage,
   stringUtilsNotExistsMessage as notExistMessage,
   stringUtilWasDeletedMessage as wasDeletedMessage,
   stringUtilWasCreatedMessage as wasCreatedMessage,
-} from 'src/utils/strings.utils';
-import { errorUtilThrowWrapper as throwWrapper } from 'src/utils/error.utils';
-import { CreateWordDto, UpdateWordDto } from 'src/api/words/models/words.dto';
-import { MFailureResponse, MSuccessResponse } from 'src/types/responses.types';
+} from "src/utils/strings.utils";
+import { errorUtilThrowWrapper as throwWrapper } from "src/utils/error.utils";
+import { CreateWordDto, UpdateWordDto } from "src/api/words/models/words.dto";
+import { MFailureResponse, MSuccessResponse } from "src/types/responses.types";
 
 @Injectable()
 export class WordsService {
   constructor(
-    @InjectModel(Word.name, 'dictDB')
+    @InjectModel(Word.name, "dictDB")
     private wordModel: Model<WordDocument>,
   ) {}
 
-  async getWord(word: string) {
+  async getSingleWord(word: string) {
     const wordDoc = await this.wordModel.findOne({
       value: word,
     });
@@ -27,12 +27,12 @@ export class WordsService {
   }
 
   async wordExists(word: string) {
-    const wordDoc = await this.getWord(word);
+    const wordDoc = await this.getSingleWord(word);
     return { state: wordDoc ? true : false, doc: wordDoc };
   }
 
   async createWord(newWord: CreateWordDto) {
-    const 
+    // const
   }
 
   async updateWord(update: UpdateWordDto, word: string) {
@@ -48,18 +48,18 @@ export class WordsService {
     return throwWrapper(
       {
         state: !wordExist.state,
-        message: notExistMessage('Word', word),
+        message: notExistMessage("Word", word),
       },
       { action },
     );
   }
 
   async deleteWord(word: string) {
-    const wordDoc = this.getWord(word);
+    const wordDoc = this.getSingleWord(word);
     if (!wordDoc) {
-      throw new Error(notExistMessage('Word', word));
+      throw new Error(notExistMessage("Word", word));
     }
     await this.wordModel.findByIdAndDelete(wordDoc);
-    return { message: wasDeletedMessage('Word', word) };
+    return { message: wasDeletedMessage("Word", word) };
   }
 }
