00822f7dc58880baa6347d86e4a99b291b9f6cce
Samuil Berenzon <samus.dev.space@gmail.com>
2024-10-02 21:44:32 +0000

    migrated changes

---
+++ libs/theme/src/provider.tsx
@@ -0,0 +1,27 @@
+import type { ReactNode } from "react";
+
+import { createTheme, MantineProvider } from "@mantine/core";
+import type { MantineColorsTuple } from "@mantine/core";
+
+const primary: MantineColorsTuple = [
+  "#e1faff",
+  "#cdf0ff",
+  "#9fdefa",
+  "#6dccf6",
+  "#46bcf2",
+  "#2cb2f1",
+  "#17adf1",
+  "#0098d7",
+  "#0087c2",
+  "#0075ac",
+];
+
+const theme = createTheme({
+  colors: {
+    primary,
+  },
+});
+
+export default function ThemeProvider({ children }: { children: ReactNode }) {
+  return <MantineProvider theme={theme}>{children}</MantineProvider>;
+}
