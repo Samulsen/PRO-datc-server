84acd5e82ecc4bb96c93c9984041e36f171f35c8
Samuil Berenzon <samus.dev.space@gmail.com>
2024-10-12 16:26:52 +0200

    more pakaages

---
+++ config/vite.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import griffel from '@griffel/vite-plugin';
+
+
+// https://vite.dev/config/
+export default defineConfig({
+    server: {
+        port: 4100,
+        host: true
+    },
+    plugins: [react(), tsconfigPaths()],
+})
