0fb3fcbe4be77931af1b812d35fec9fa22421cfc
Samuil Berenzon <samus.dev.space@gmail.com>
2024-10-18 13:32:29 +0000

    rm

--- apps/frontend/admin/src/main.tsx
+++ apps/frontend/admin/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 
 import App from "@app-admin/App";
 
+// NOTE: probably should have try catch in case root does not exist (unlikely)
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
