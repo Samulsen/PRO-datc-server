50378f51b92678ff66b37f28cf0b62195d46f9a4
Samuil Berenzon <samus.dev.space@gmail.com>
2024-11-14 20:41:08 +0000

    rm comments

--- config/ci/unit-tests/caller.mk
+++ config/ci/unit-tests/caller.mk
@@ -1,18 +1,14 @@
 p=unit-tests-
 runner=npx ts-node config/ci/unit-tests/runner.ts
 
-# start the unit tests for the app: ui from the frontend domain
 $(p)app-frontend-ui:
 	@$(runner) frontendUI
 
-# start the unit tests for the app: admin from the frontend domain
 $(p)app-frontend-admin:
 	@$(runner) frontendAdmin
 
-# start the unit tests for the library: components from the frontend domain
 $(p)lib-components:
 	@$(runner) libComponents
 
-# start the unit tests for the library: theme from the frontend domain
 $(p)lib-theme:
 	@$(runner) libTheme
