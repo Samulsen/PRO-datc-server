baf7bb5685ff817329e9f8008206a9ddeeb739a9
Samuil Berenzon <samus.dev.space@gmail.com>
2024-11-19 19:35:26 +0000

    new pack

--- config/ci/runner.ts
+++ config/ci/runner.ts
@@ -1,35 +1,12 @@
 import chalk = require("chalk");
 import cliProgress = require("cli-progress");
 
-import runCompile from "./steps/compile/def";
-import runTest from "./steps/unit-tests/def";
-import runLint from "./steps/lint/def";
-
-import { selectTarget, targetMap } from "./helpers/constants";
-
-type TTarget = keyof typeof targetMap;
+import runTargetWithAllSteps from "./helpers/runtTargetWithAllSteps";
+import { targetMap, TTarget } from "./helpers/constants";
 
 const targets = Object.keys(targetMap) as TTarget[];
 
 const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-
-async function runTargetWithAllSteps(target: TTarget) {
-  const { tag } = selectTarget(target, "src");
-  console.log("\n");
-  console.log(chalk.blue("\nCurrent:"), chalk.yellowBright(tag));
-  await Promise.all([
-    runLint(target, "src", true),
-    runCompile(target, "root", true),
-    runTest(target, "src", true),
-  ]).then(() => {
-    return new Promise<void>((resolve) => {
-      setTimeout(() => {
-        console.clear();
-        resolve();
-      }, 1000);
-    });
-  });
-}
 
 async function runAllTargets() {
   console.log(chalk.blue("Running CI on all targets..."));
