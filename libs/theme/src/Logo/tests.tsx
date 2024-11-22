16196f7c63505c713ea8ccf0dd36d7c1a879220f
Samuil Berenzon <samus.dev.space@gmail.com>
2024-11-22 20:30:18 +0000

    added logo tets

--- libs/theme/src/Logo/tests.tsx
+++ libs/theme/src/Logo/tests.tsx
@@ -9,7 +9,6 @@ describe("Logo", () => {
     render(<Logo />);
     it("should render with medium dimensions and as filled", () => {
       const LogoElement = screen.getByTestId(logoConst.dataTestId);
-      expect(LogoElement).toBeInTheDocument();
 
       expect(LogoElement).toHaveStyle("width: 32px");
       expect(LogoElement).toHaveStyle("height: 32px");
@@ -19,7 +18,6 @@ describe("Logo", () => {
     it("should render with extraSmall dimensions", () => {
       render(<Logo size="extraSmall" />);
       const LogoElement = screen.getByTestId(logoConst.dataTestId);
-      expect(LogoElement).toBeInTheDocument();
 
       expect(LogoElement).toHaveStyle("width: 16px");
       expect(LogoElement).toHaveStyle("height: 16px");
@@ -27,15 +25,20 @@ describe("Logo", () => {
     it("should render with small dimensions", () => {
       render(<Logo size="small" />);
       const LogoElement = screen.getByTestId(logoConst.dataTestId);
-      expect(LogoElement).toBeInTheDocument();
 
       expect(LogoElement).toHaveStyle("width: 24px");
       expect(LogoElement).toHaveStyle("height: 24px");
     });
+    it("should render with medium dimensions", () => {
+      render(<Logo size="medium" />);
+      const LogoElement = screen.getByTestId(logoConst.dataTestId);
+
+      expect(LogoElement).toHaveStyle("width: 32px");
+      expect(LogoElement).toHaveStyle("height: 32px");
+    });
     it("should render with large dimensions", () => {
       render(<Logo size="large" />);
       const LogoElement = screen.getByTestId(logoConst.dataTestId);
-      expect(LogoElement).toBeInTheDocument();
 
       expect(LogoElement).toHaveStyle("width: 64px");
       expect(LogoElement).toHaveStyle("height: 64px");
@@ -43,10 +46,35 @@ describe("Logo", () => {
     it("should render with extraLarge dimensions", () => {
       render(<Logo size="extraLarge" />);
       const LogoElement = screen.getByTestId(logoConst.dataTestId);
-      expect(LogoElement).toBeInTheDocument();
 
       expect(LogoElement).toHaveStyle("width: 96px");
       expect(LogoElement).toHaveStyle("height: 96px");
+    });
+  });
+  describe("with appearance prop", () => {
+    it("should render as filled", () => {
+      render(<Logo appereance="filled" />);
+      const LogoElement = screen.getByTestId(logoConst.dataTestId);
+      const svgString = LogoElement.innerHTML;
+      expect(svgString).toContain(`fill="#2BA5E7"`);
+      expect(svgString).toContain(`fill="#EF5198"`);
+      expect(svgString).toContain(`fill="#32B9A6"`);
+      expect(svgString).toContain(`fill="#FAC710"`);
+    });
+    it("should render as outlined", () => {
+      render(<Logo appereance="outlined" />);
+      const LogoElement = screen.getByTestId(logoConst.dataTestId);
+      const svgString = LogoElement.innerHTML;
+      expect(svgString).toContain(`stroke="#2BA5E7"`);
+      expect(svgString).toContain(`stroke="#EF5198"`);
+      expect(svgString).toContain(`stroke="#32B9A6"`);
+      expect(svgString).toContain(`stroke="#FAC710"`);
+    });
+    it("should render as outlined-neutral", () => {
+      render(<Logo appereance="outlined-neutral" />);
+      const LogoElement = screen.getByTestId(logoConst.dataTestId);
+      const svgString = LogoElement.innerHTML;
+      expect(svgString).toContain(`stroke="#F5F5F5"`);
     });
   });
 });
---
+++ libs/theme/src/Provider/tests.tsx
@@ -0,0 +1,6 @@
+import { render, screen } from "@tests-unit-browser";
+import "@testing-library/jest-dom";
+
+describe("Provider", () => {
+  it("should render", () => {});
+});
