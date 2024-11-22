68dd8ffd9a126f4cc26db45ac4694e511921d8ee
Samuil Berenzon <samus.dev.space@gmail.com>
2024-11-22 20:56:06 +0000

    added test for func

--- libs/theme/src/Provider/tests.tsx
+++ libs/theme/src/Provider/tests.tsx
@@ -1,6 +1,18 @@
 import { render, screen } from "@tests-unit-browser";
 import "@testing-library/jest-dom";
 
-describe("Provider", () => {
-  it("should render", () => {});
+import ThemeProvider from "@lib-theme/Provider/func";
+
+describe("ThemeProvider", () => {
+  it("should render the provider node with correct className for useFuiProviderNode hook", () => {
+    render(<ThemeProvider>Test</ThemeProvider>);
+    const ProviderNodeByChildren = screen.getByText("Test");
+
+    const ProviderNodeByClassName =
+      document.getElementsByClassName("fui-FluentProvider");
+
+    expect(ProviderNodeByChildren).toBeInTheDocument();
+    // strict mode makes is so two providers are rendered
+    expect(ProviderNodeByClassName).toHaveLength(2);
+  });
 });
