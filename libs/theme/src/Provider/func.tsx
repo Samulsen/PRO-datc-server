89e248b9b037a86cbdb431a0fe78a15837464a02
Samuil Berenzon <samus.dev.space@gmail.com>
2024-12-30 19:35:22 +0000

    missing font overide

--- libs/theme/src/Provider/func.tsx
+++ libs/theme/src/Provider/func.tsx
@@ -4,6 +4,8 @@ import { FluentProvider } from "@fluentui/react-components";
 import colors from "@lib-theme/Provider/colors";
 import { useClasses, useStaticStyles } from "@lib-theme/Provider/styles";
 
+import "./font-face.css";
+
 export default function ThemeProvider({ children }: { children: ReactNode }) {
   useStaticStyles();
 
