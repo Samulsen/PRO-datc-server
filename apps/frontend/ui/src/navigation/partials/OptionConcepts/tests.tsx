8ea19e30d917d2543d1f42f91774eb1cd51e2c74
Samuil Berenzon <samus.dev.space@gmail.com>
2025-01-30 20:14:37 +0000

    update

--- apps/frontend/ui/src/navigation/partials/OptionConcepts/tests.tsx
+++ apps/frontend/ui/src/navigation/partials/OptionConcepts/tests.tsx
@@ -4,7 +4,7 @@ import "@testing-library/jest-dom";
 import OptionConcepts from "@app-ui/navigation/partials/OptionConcepts";
 
 describe("OptionConcepts", () => {
-  it("should render with given concepts, without overflow", () => {
+  it("should render with given concepts", () => {
     const onSearch = jest.fn((value: string) => value);
 
     render(
@@ -16,8 +16,24 @@ describe("OptionConcepts", () => {
 
     const menuList = screen.getByRole("menu");
     expect(menuList).toBeInTheDocument();
-    const isScrollable = menuList.scrollHeight > menuList.clientHeight;
-    expect(isScrollable).toBe(false);
+
+    const concept1 = screen.getByText("concept1");
+    expect(concept1).toBeInTheDocument();
+    const concept2 = screen.getByText("concept2");
+    expect(concept2).toBeInTheDocument();
+    const concept3 = screen.getByText("concept3");
+    expect(concept3).toBeInTheDocument();
+  });
+
+  it("should be able to select different concepts", () => {
+    const onSearch = jest.fn((value: string) => value);
+
+    render(
+      <OptionConcepts
+        concepts={["concept1", "concept2", "concept3"]}
+        onSearch={onSearch}
+      />,
+    );
 
     const concept1 = screen.getByText("concept1");
     expect(concept1).toBeInTheDocument();
@@ -43,52 +59,6 @@ describe("OptionConcepts", () => {
     fireEvent.click(concept3);
     fireEvent.click(searchButton);
     expect(onSearch).toHaveBeenCalledWith("concept3");
-
-    expect(onSearch).toHaveBeenCalledTimes(3);
-  });
-
-  it("should render with given concepts, with overflow", () => {
-    const onSearch = jest.fn((value: string) => value);
-
-    render(
-      <OptionConcepts
-        concepts={Array.from(
-          { length: 100 },
-          (_, i) => `concept${i.toString()}`,
-        )}
-        onSearch={onSearch}
-      />,
-    );
-
-    const menuList = screen.getByRole("menu");
-    expect(menuList).toBeInTheDocument();
-    const isScrollable = menuList.scrollHeight > menuList.clientHeight;
-    expect(isScrollable).toBe(true);
-
-    const concept1 = screen.getByText("concept1");
-    expect(concept1).toBeInTheDocument();
-    const concept50 = screen.getByText("concept50");
-    expect(concept50).toBeInTheDocument();
-    const concept100 = screen.getByText("concept99");
-    expect(concept100).toBeInTheDocument();
-
-    const searchButton = screen.getByRole("button", { name: "Search" });
-    expect(searchButton).toBeInTheDocument();
-    expect(searchButton).toBeDisabled();
-
-    fireEvent.click(concept1);
-    expect(searchButton).toBeEnabled();
-
-    fireEvent.click(searchButton);
-    expect(onSearch).toHaveBeenCalledWith("concept1");
-
-    fireEvent.click(concept50);
-    fireEvent.click(searchButton);
-    expect(onSearch).toHaveBeenCalledWith("concept50");
-
-    fireEvent.click(concept100);
-    fireEvent.click(searchButton);
-    expect(onSearch).toHaveBeenCalledWith("concept99");
 
     expect(onSearch).toHaveBeenCalledTimes(3);
   });
